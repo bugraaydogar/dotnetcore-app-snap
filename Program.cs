7ec8a01e0099b0c829f312bdb4fbf1a67fc3afa2
Buğra Aydoğar <bugraaydogar@gmail.com>
2021-09-14 13:23:29 +0300

    An initial snapped hello-world app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace dotnet.hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
